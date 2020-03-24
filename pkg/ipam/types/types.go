a0590a45a1725d575a403b7a4c0e8ec8901a5087
André Martins <andre@cilium.io>
2020-03-24 21:11:46 +0100

    add deepcopy for pkg/k8s/types
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/ipam/types/types.go
+++ pkg/ipam/types/types.go
@@ -219,6 +219,8 @@ type PoolQuota struct {
 type PoolQuotaMap map[PoolID]PoolQuota
 
 // Interface is the implementation of a IPAM relevant network interface
+// +k8s:deepcopy-gen=false
+// +deepequal-gen=false
 type Interface interface {
 	// InterfaceID must return the identifier of the interface
 	InterfaceID() string
@@ -233,6 +235,7 @@ type Interface interface {
 // and the resource itself.
 //
 // +k8s:deepcopy-gen=false
+// +deepequal-gen=false
 type InterfaceRevision struct {
 	// Resource is the interface resource
 	Resource Interface
@@ -248,6 +251,7 @@ type InterfaceRevision struct {
 // per-node IPAM logic
 //
 // +k8s:deepcopy-gen=false
+// +deepequal-gen=false
 type Instance struct {
 	// interfaces is a map of all interfaces attached to the instance
 	// indexed by the interface ID
@@ -257,6 +261,7 @@ type Instance struct {
 // InstanceMap is the list of all instances indexed by instance ID
 //
 // +k8s:deepcopy-gen=false
+// +deepequal-gen=false
 type InstanceMap struct {
 	mutex lock.RWMutex
 	data  map[string]*Instance
